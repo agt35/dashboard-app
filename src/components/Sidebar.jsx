e519a1959fde0b5eab9d42865101af4a2e8d1ce7
Alfonso <agponchito@hotmail.com>
2023-06-29 10:42:23 -0600

    Small aesthetic changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,18 @@
 import { NavLink } from "react-router-dom";
-import { useLocation } from "react-router-dom";
-import { useEffect } from "react";
 
+import SendRoundedIcon from "@mui/icons-material/SendRounded";
 import TroubleshootTwoToneIcon from "@mui/icons-material/TroubleshootTwoTone";
 import FlagTwoToneIcon from "@mui/icons-material/FlagTwoTone";
 import MyLocationTwoToneIcon from "@mui/icons-material/MyLocationTwoTone";
 import CompareArrowsTwoToneIcon from "@mui/icons-material/CompareArrowsTwoTone";
 
 const Sidebar = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    console.log(location.pathname);
-  }, [location]);
   return (
     <div className="h-screen w-[18%] bg-blue-900 shadow-2xl rounded-sm z-10 flex flex-col font-roboto">
-      <h1 className="text-zinc-200 text-5xl text-center h-[10%] mt-10">
-        Flight Plans
-      </h1>
+      <div className="flex flex-col justify-center items-center py-5 bg-blue-300/25">
+        <SendRoundedIcon sx={{ fontSize: 100 }} className="text-zinc-200" />
+        <h1 className="text-zinc-200 text-5xl text-center">Flight Plans</h1>
+      </div>
       <div className="mt-8 flex flex-col items-center">
         <div className="text-zinc-400 p-6 text-2xl text-center flex hover:text-blue-200 hover:bg-blue-500/25 w-full justify-center">
           <NavLink
--- src/screens/OverviewScreen.jsx
+++ src/screens/OverviewScreen.jsx
@@ -1,7 +1,7 @@
 const OverviewScreen = () => {
   return (
     <div className="font-roboto flex flex-col justify-center items-center mt-2">
-      <h2 className="text-6xl text-zinc-400">Overview</h2>
+      <h2 className="text-6xl text-blue-100">Overview</h2>
     </div>
   );
 };
