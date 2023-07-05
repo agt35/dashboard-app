9baf5a0d885b61077ac3144bc40dbd68b52bead1
Alfonso <agponchito@hotmail.com>
2023-07-05 13:06:11 -0600

    Modify colors available for charts

--- src/data/utils.js
+++ src/data/utils.js
@@ -1,15 +1,41 @@
+// import axios from "axios";
+
+// const getData = async () => {
+//   try {
+//     const response = await axios.get("http://127.0.0.1:8000/data_region");
+//     return response.data;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
 export const getLabels = (data) => {
   const periods = [...new Set(data.map((item) => item.period))];
   return periods;
 };
 
 export const getDatasets = (data) => {
+  const colors = [
+    "#e5e5e5",
+    "#fecaca",
+    "#b91c1c",
+    "#fbbf24",
+    "#a3e635",
+    "#15803d",
+    "#99f6e4",
+    "#22d3ee",
+    "#0369a1",
+    "#7c3aed",
+    "#d946ef",
+    "#581c87",
+    "#f9a8d4",
+  ];
   const dataset = [];
+  console.log(data);
   const labels = [...new Set(data.map((item) => item.name))];
   labels.forEach((label) => {
-    const color = `rgba(${Math.floor(Math.random() * 255)},${Math.floor(
-      Math.random() * 255
-    )},${Math.floor(Math.random() * 255)},0.8)`; // random color
+    const randomPosition = Math.floor(Math.random() * colors.length);
+    const color = colors.splice(randomPosition, 1);
     const labelData = data.filter((item) => item.name === label);
     const labelDataset = {
       label: label,
@@ -28,3 +54,6 @@ export const getDatasets = (data) => {
   });
   return dataset;
 };
+
+// const data = await getData();
+// console.log(getDatasets(data));
